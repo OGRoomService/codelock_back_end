b48835706eaf42fb6bf4acdde90bf584a2c2ab94
OGRoomService <51579862+OGRoomService@users.noreply.github.com>
2021-10-19 00:02:14 -0400

    Initial Build

---
+++ src/main/java/com/codelock/codelock/CodelockApplication.java
@@ -0,0 +1,13 @@
+package com.codelock.codelock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodelockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodelockApplication.class, args);
+	}
+
+}
